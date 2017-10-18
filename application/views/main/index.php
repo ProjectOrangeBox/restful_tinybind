ab970d7c860b8ae803a6ef155e552f1dd5c3918d
Don Myers <donmyers@quadratec.com>
2017-10-18 13:51:50 -0400

    update welcome view

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,9 +1,9 @@
 <? page::extends('_templates/orange_default') ?>
 
 <? page::section('section_container') ?>
-<h1>Welcome!</h1>
-<p><a href="<?=site_url('{login}') ?>">Login</a></p>
 
-<p>Username: <?=user::username() ?></p>
+<h1>Welcome <small><?=user::username() ?></small></h1>
+
+<p><a href="<?=site_url('{login}') ?>">Login</a></p>
 
 <? page::end() ?>
