555df70ea96804256fa1a38d4a52190b69102b37
Don Myers <donmyers@quadratec.com>
2017-12-21 12:11:38 -0500

    add migrations

---
+++ support/migrations/001_init.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 20171218114855_init */
+
+class Migration_Init extends CI_Migration {
+
+	public function up() {
+	}
+	
+	public function down() {
+	}
+
+} /* end migration */
---
+++ support/migrations/002_quad_backorder_mgr_history.php
@@ -0,0 +1,25 @@
+<?php 
+
+/* 002_quad_backorder_mgr_history */
+
+class Migration_Quad_backorder_mgr_history extends CI_Migration {
+
+	public function up() {
+$sql = <<<EOF
+	CREATE TABLE `quad_backorder_mgr_history` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created` timestamp NULL DEFAULT current_timestamp(),
+  `item_no_core` varchar(32) DEFAULT 'NULL',
+  `entry` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+EOF;
+	
+		$this->db->query($sql);
+	}
+	
+	public function down() {
+		$this->dbforge->drop_table('quad_backorder_mgr_history');
+	}
+
+} /* end migration */
