b8feb190a8b6fe4cc11005d85ce16b3a87156d02
Don Myers <donmyers@quadratec.com>
2017-12-22 14:24:21 -0500

    update

--- application/views/admin/dashboard/index_block.php
+++ application/views/admin/dashboard/index_block.php
@@ -1,11 +1,10 @@
 	<?php if (user::can($__0)) { ?>
-  <div class="col-xs-12 col-sm-6 col-md-3 col-lg-2">
-		<a href="<?=$__1 ?>" class="dashboard-block">
-			<div class="dashboard-block">
-				<i class="fa fa-cogs fa-2x" aria-hidden="true"></i>
-				<br>
+  <div class="col-xs-12 col-sm-6 col-md-3 col-lg-2 col-margin-tb">
+		<a href="<?=$__1 ?>" class="btn btn-primary btn-lg btn-block">
+			<i class="fa fa-cogs fa-2x" aria-hidden="true"></i>
+			<br>
 				<?=$__2 ?>
-			</div>
 		</a>
   </div>
   <?php } ?>
+
