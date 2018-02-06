320b3b383316531719fc65d76285b15c1f315a66
Don Myers <donmyers@quadratec.com>
2018-02-06 14:55:54 -0500

    dashboard

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -10,17 +10,18 @@
 <div class="row">
 
 	<? pear::include('/main/index_block',['','/backorder-call-center','Backorder<br>Search']) ?>
-
 	<? pear::include('/main/index_block',['','/dropships','Dropship<br>Search']) ?>
 
 </div>
-<div class="row admin-row">
 
-	<? pear::include('/main/index_block',['url::/admin/backorder::index~get','/admin/backorder','Backorder<br>MGR','user-circle-o']) ?>
+<div class="row">
+	<div class="col-md-12"><h4><i class="fa fa-user-circle-o"></i> Admin</h4></div>
 
-	<? pear::include('/main/index_block',['url::/admin/backorder_buyer::index~get','/admin/backorder_buyer','Backorder<br>Buyer Groups','user-circle-o']) ?>
+	<? pear::include('/main/index_block',['url::/admin/backorder::index~get','/admin/backorder','Backorder<br>MGR','industry','265476']) ?>
 
-	<? pear::include('/main/index_block',['url::/admin/dropships::index~get','/admin/dropships','Drop Ship<br>MGR','user-circle-o']) ?>
+	<? pear::include('/main/index_block',['url::/admin/backorder_buyer::index~get','/admin/backorder_buyer','Backorder<br>Buyer Groups','list','265476']) ?>
+
+	<? pear::include('/main/index_block',['url::/admin/dropships::index~get','/admin/dropships','Drop Ship<br>MGR','industry','487626']) ?>
 
 </div>
 
@@ -30,15 +31,6 @@
 
 <? pear::section('page_style') ?>
 .dashboard a.btn.btn-lg.btn-block {
-	background-color: #0b6854;
-	color: white;
-	transition:all 0.3s ease;
-  opacity:0.9;
-  color: white;
-}
-.dashboard .admin-row a.btn.btn-lg.btn-block {
-	background-color: #265476;
-	color: white;
 	transition:all 0.3s ease;
   opacity:0.9;
   color: white;
@@ -48,6 +40,6 @@
   -ms-transform: scale(1.05);
   transform: scale(1.02);
   opacity:1;
-  color: #fcd05c;
+  color: #eee;
 }
 <? pear::end() ?>
