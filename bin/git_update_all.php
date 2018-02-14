820754ab18b84b69227eea5633256a2f58bdadcc
Don Myers <donmyers@quadratec.com>
2018-02-14 14:06:29 -0500

    add git update

---
+++ bin/git_update_all.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+
+passthru('sudo echo');
+
+define('ROOTPATH',realpath(__DIR__.'/../'));
+
+$_ENV = $_ENV + require ROOTPATH.'/_env';
+
+if (!isset($_ENV['GITBRANCH'])) {
+	die('GIT Branch Missing'.chr(10));
+}
+
+echo 'Using branch '.$_ENV['GITBRANCH'].chr(10);
+
+exec('find '.s(ROOTPATH).' -name .git',$repros);
+
+foreach ($repros as $repro) {
+	$repro = dirname($repro);
+
+	chdir($repro);
+
+	echo $repro.chr(10);
+
+	passthru('git fetch --all');
+	passthru('git reset --hard origin/'.$_ENV['GITBRANCH']);
+}
+
+function s($input) {
+	return str_replace(' ','\ ',$input);
+}
