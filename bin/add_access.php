658803ff2b5fa67e1cb67ec76dd143cca0854131
Don Myers <donmyers@quadratec.com>
2018-02-19 13:29:49 -0500

    clean up deploy

--- bin/add_access.php
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-require 'support.inc.php';
-
-passthru('cd '.ESCROOTPATH.'/public;php index.php cli/auto_add_permissions');
--- bin/migrate.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-require 'support.inc.php';
-
-$dir = (isset($_SERVER['argv'][1])) ? $_SERVER['argv'][1] : 'latest';
-
-shell('cd '.ESCROOTPATH.'/public;php index.php cli/migrate/'.$dir);
