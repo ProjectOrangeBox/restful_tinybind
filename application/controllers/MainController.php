d8d58ca86393c9ceb244e1c77b52a034752caddb
Don Myers <donmyers@quadratec.com>
2018-06-05 12:07:28 -0400

    remove old hijack

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -6,29 +6,4 @@ class MainController extends MY_Controller {
 		ci('page')->render();
 	}
 
-	public function hijackAction($key=null) {
-		assert(ctype_xdigit($key));
-
-		$parts = explode(chr(0),hex2bin($key));
-
-		/* must be 3 parts */
-		if (count($parts) != 3) {
-			ci('errors')->display(403);
-		}
-
-		/* greater than 10 minutes */
-		if (date('U') - $parts[1] > 600) {
-			ci('errors')->display(403);
-		}
-
-		/* check hmac */
-		if ($parts[2] !== md5($parts[0].chr(0).$parts[1].chr(0).ci('config')->item('encryption_key'))) {
-			ci('errors')->display(403);
-		}
-
-		ci('auth')->refresh_userdata((int)$parts[0]);
-
-		redirect('{dashboard}');
-	}
-
 } /* end class */
