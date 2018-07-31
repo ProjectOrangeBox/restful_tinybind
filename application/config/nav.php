87b32df0b3d8d6df50907d0d18f7c3dd13872084
Don Myers <donmyers@quadratec.com>
2018-07-31 13:46:57 -0400

    nav updates

--- application/config/nav.php
+++ application/config/nav.php
@@ -4,6 +4,9 @@
 $config['left'] = 1;
 $config['right'] = 2;
 
+$config['right protected'] = 58;
+$config['right public'] = 57;
+
 /* default styles - used in migrations */
 $config['styles'] = [
 	'protected'=>['icon'=>'user-secret','color'=>'00007F'],
@@ -13,8 +16,8 @@ $config['styles'] = [
 ];
 
 $config['bootstrap nav'] = [
-	'navigation_open'=>'<ul class="nav navbar-nav">',
-	'navigation_close'=>'</ul>',
+	'navigation_open'=>'',
+	'navigation_close'=>'',
 	
 	'item_open'=>'<li>',
 	'item_close'=>'</li>',
@@ -28,9 +31,34 @@ $config['bootstrap nav'] = [
 	'anchor_dropdown'=>'<a href="{url}" class="{class} dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{text}</a>',
 	'anchor_sub_dropdown'=>'<a href="{url}" class="{class} dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{text}</a>',
 	
+	'hr'=>'<li role="separator" class="divider"></li>',
+	
 	'dropdown_open'=>'<ul class="dropdown-menu" role="menu">',
 	'dropdown_close'=>'</ul>',
 ];
+
+$config['bootstrap nav icons'] = [
+	'navigation_open'=>'',
+	'navigation_close'=>'',
+	
+	'item_open'=>'<li>',
+	'item_close'=>'</li>',
+
+	'item_open_dropdown'=>'<li class="dropdown">',
+	'item_close_dropdown'=>'</li>',
+	
+	'item_open_dropdown_sub'=>'<li class="dropdown dropdown-submenu">',
+	
+	'anchor'=>'<a href="{url}" data-color="{color}" data-icon="{icon}" target="{target}"><i class="fa fa-{icon}"></i> {text}</a>',
+	'anchor_dropdown'=>'<a href="{url}" class="{class} dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-{icon}"></i> {text}</a>',
+	'anchor_sub_dropdown'=>'<a href="{url}" class="{class} dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-{icon}"></i> {text}</a>',
+
+	'hr'=>'<li role="separator" class="divider"></li>',
+	
+	'dropdown_open'=>'<ul class="dropdown-menu" role="menu">',
+	'dropdown_close'=>'</ul>',
+];
+
 
 $config['dd-list'] = [
 	'navigation_open'=>'<ol class="dd-list">',
--- application/views/_templates/nav.php
+++ application/views/_templates/nav.php
@@ -9,39 +9,28 @@
 			</button>
 			<a class="navbar-brand" href="/">Q</a>
 		</div>
+		
 		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
 			<?=ci('nav_library')->build_bootstrap_nav(config('nav.left'),config('nav.bootstrap nav')) ?>
+			</ul>
+		
 			<ul class="nav navbar-nav navbar-right">
+				
 				<? if (user::has_permission('url::/orange_user_msgs::index~get')) { ?>
 				<li>
 					<a href="/admin/msgs"><i class="fa fa-envelope"></i> <span class="badge"><?=pear::user_messages(user::id()) ?></span></a>
 				</li>
 				<? } ?>
-				<li>
+				
 				<? if (user::logged_in()) { ?>
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-						<img alt="" class="img-circle" src="https://www.gravatar.com/avatar/<?=md5(strtolower(trim(user::email()))) ?>?s=32" />
-						<span class="username username-hide-on-mobile"> <?=user::username() ?></span> <span class="caret"></span>
-					</a>
+					<?=ci('nav_library')->build_bootstrap_nav(config('nav.right protected'),config('nav.bootstrap nav icons'),false) ?>
 				<? } else { ?>
-					<a href="<?=site_url('{login}') ?>">
-						<i class="fa fa-sign-in"></i> Login
-					</a>
+					<?=ci('nav_library')->build_bootstrap_nav(config('nav.right public'),config('nav.bootstrap nav icons'),false) ?>
 				<? } ?>
-				<ul class="dropdown-menu">
-					<? if (user::has_one_permission_of('menu::edit profile')) { ?>
-						<li><a href="/users/edit-profile/<?=user::id() ?>"><i class="fa fa-user"></i> My Profile</a></li>
-					<? } ?>
-					<? foreach (user::roles() as $role) { ?>
-						<li class="disabled"><a tabindex="-1" href="#"><i class="fa fa-users"></i> <?=$role ?></a></li>
-					<? } ?>
-					<li role="separator" class="divider"></li>
-					<? if (user::logged_in()) { ?>
-						<li><a href="<?=site_url('{logout}') ?>"><i class="fa fa-key"></i> Log Out</a></li>
-					<? } ?>
-				</ul>
-				</li>
-				</ul>
+			
+			</ul>
 		</div>
+
 	</div>
 </nav>
