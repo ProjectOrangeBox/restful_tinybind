5275043cca6866dbea4c470cba9524f5682337ff
Don Myers <donmyers@quadratec.com>
2018-08-07 14:17:19 -0400

    updates

--- application/config/ldap.php
+++ application/config/ldap.php
@@ -1,8 +1,11 @@
 <?php
 
 $config['account_suffix'] = '@quadratec.local';
-$config['domain_controllers'] = ['quad-dc2.quadratec.local','quad-dc1.quadratec.local'];
+$config['domain_controllers'] = [env('ldap.secondary'),env('ldap.primary')];
 $config['base_dn'] = 'DC=quadratec,DC=local'; // CN=Skynet,OU=Information Systems,DC=quadratec,DC=local
 
 $config['admin_username'] = 'skynet_admin';
 $config['admin_password'] = 'FDCQk}\'n';
+
+/* 172.16.62.205 172.16.62.158 */
+/* 'quad-dc2.quadratec.local','quad-dc1.quadratec.local' */
