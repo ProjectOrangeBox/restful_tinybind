f0792de1d0606687175470d752765c7f11fb9e4e
Don Myers <donmyers@quadratec.com>
2018-08-09 13:25:36 -0400

    add 404 to main controller

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -1,5 +1,10 @@
 <?php
 
 class MainController extends MY_Controller {
-	use admin_index_render_controller_trait;	
+	use admin_index_render_controller_trait;
+
+	public function route404Action() {
+		show_404();
+	}
+
 } /* end class */
