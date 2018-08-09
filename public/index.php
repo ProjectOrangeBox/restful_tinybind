29ea4e668cbf2e7dfa8aefd30373c4d0cabaaf6c
Don Myers <donmyers@quadratec.com>
2018-08-09 14:09:25 -0400

    shorten up index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CodeIgniter
  *
@@ -81,12 +82,8 @@ if (!file_exists(ROOTPATH.'/.env')) {
 	die('.env file missing');
 }
 
-/* leave this until all .env files are converted to true ini format */
-$env_content = file_get_contents(ROOTPATH.'/.env');
-$env = (substr($env_content,0,5) == '<?php') ? require ROOTPATH.'/.env' : parse_ini_string($env_content,true,INI_SCANNER_TYPED);
-
-/* bring in the system _env files */
-$_ENV = $_ENV + $env;
+/* bring in the system .env files */
+$_ENV = $_ENV + parse_ini_file(ROOTPATH.'/.env',true,INI_SCANNER_TYPED);
 
 /* if phpunit then setup empty argument as empty array so main/index loads */
 if (isset($_ENV['phpunit'])) {
@@ -137,7 +134,8 @@ switch ($_ENV['SERVER_DEBUG']) {
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
-	$system_path = '../vendor/codeigniter/framework/system';
+
+$system_path = '../vendor/codeigniter/framework/system';
 
 /*
  *---------------------------------------------------------------
@@ -154,7 +152,8 @@ switch ($_ENV['SERVER_DEBUG']) {
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = '../application';
+
+$application_folder = '../application';
 
 /*
  *---------------------------------------------------------------
@@ -169,38 +168,8 @@ switch ($_ENV['SERVER_DEBUG']) {
  *
  * NO TRAILING SLASH!
  */
-	$view_folder = '';
 
-
-/*
- * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
- * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here. For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT: If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller. Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
- */
-	// The directory name, relative to the "controllers" directory.  Leave blank
-	// if your controller is not in a sub-directory within the "controllers" one
-	// $routing['directory'] = '';
-
-	// The controller class file name.  Example:  mycontroller
-	// $routing['controller'] = '';
-
-	// The controller function you wish to be called.
-	// $routing['function']	= '';
-
+$view_folder = $application_folder.'/views';
 
 /*
  * -------------------------------------------------------------------
@@ -230,120 +199,47 @@ switch ($_ENV['SERVER_DEBUG']) {
  * ---------------------------------------------------------------
  */
 
-	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
-
-	if (($_temp = realpath($system_path)) !== FALSE)
-	{
-		$system_path = $_temp.DIRECTORY_SEPARATOR;
-	}
-	else
-	{
-		// Ensure there's a trailing slash
-		$system_path = strtr(
-			rtrim($system_path, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		).DIRECTORY_SEPARATOR;
-	}
-
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-		exit(3); // EXIT_CONFIG
-	}
+// Is the system path correct?
+if (!$system_path = realpath($system_path)) {
+	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+	echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
+	exit(3); // EXIT_CONFIG
+}
 
 /*
  * -------------------------------------------------------------------
  *  Now that we know the path, set the main path constants
  * -------------------------------------------------------------------
  */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-	// Path to the system directory
-	define('BASEPATH', $system_path);
+// The name of THIS file
+define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
 
-	// Path to the front controller (this file) directory
-	define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
+// Path to the system directory
+define('BASEPATH', $system_path.DIRECTORY_SEPARATOR);
 
-	// Name of the "system" directory
-	define('SYSDIR', basename(BASEPATH));
+// Path to the front controller (this file) directory
+define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
-	// The path to the "application" directory
-	if (is_dir($application_folder))
-	{
-		if (($_temp = realpath($application_folder)) !== FALSE)
-		{
-			$application_folder = $_temp;
-		}
-		else
-		{
-			$application_folder = strtr(
-				rtrim($application_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
-		}
-	}
-	elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
-	{
-		$application_folder = BASEPATH.strtr(
-			trim($application_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
-	}
-	else
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
-	}
+// Name of the "system" directory
+define('SYSDIR', basename(BASEPATH));
 
-	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
 
-	// The path to the "views" directory
-	if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.'views';
-	}
-	elseif (is_dir($view_folder))
-	{
-		if (($_temp = realpath($view_folder)) !== FALSE)
-		{
-			$view_folder = $_temp;
-		}
-		else
-		{
-			$view_folder = strtr(
-				rtrim($view_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
-		}
-	}
-	elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.strtr(
-			trim($view_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
-	}
-	else
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
-	}
+if (!$application_folder = realpath($application_folder)) {
+	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+	echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+	exit(3); // EXIT_CONFIG
+}
 
-	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
+define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+
+if (!$view_folder = realpath($view_folder)) {
+	header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+	echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
+	exit(3); // EXIT_CONFIG
+}
+
+define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
 
 /* orange methods */
 require_once ORANGEPATH.'/core/Orange.php';
