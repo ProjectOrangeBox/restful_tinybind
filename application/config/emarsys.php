c80ae203773b41b941a7a8d13a71aca31db12f36
Don Myers <donmyers@icloud.com>
2019-01-03 13:47:06 -0500

    remove emarsys config use emarsys  settings

--- application/config/emarsys.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$config['username'] = 'quadratec002';
-$config['password'] = 'uWfR33yOLKxB06IfIYl2';
-
-$config['suite_api_url'] = 'https://api.emarsys.net/api/v2/';
-
-$config['order www product'] = 'http://www.quadratec.com/products/%%.htm';
-$config['order www image'] = 'http://www.quadratec.com/sites/default/files/styles/product_small/public/product_images/%%.jpg';
