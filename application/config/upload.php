bd00a2bf4b4e4a9151dbc3a8998b4cb9be2faf10
Don Myers <donmyers@icloud.com>
2019-01-21 10:29:47 -0500

    update configs

--- application/config/upload.php
+++ application/config/upload.php
@@ -1,14 +1,24 @@
 <?php
 
 $config['upload_path'] = ROOTPATH.'/var/uploads/';
-$config['allowed_types'] = 'gif|jpg|png';
+$config['allowed_types'] = ['gif','jpg','png'];
 $config['max_size'] = '9000';
 
 $config['max_width'] = '2000';
 $config['max_height'] = '2000';
 
 $config['move_to'] = WWW.'/images/';
+$config['strip folder'] = WWW;
 
-$config['excel_src'] = [
-	'allowed_types'=>'xls|xlsx',
+/**
+ * 
+ * This array matches the name of a input field
+ * this is merged with the base configuration array will additional
+ * configuration for this input field
+ *
+ */
+$config['filea'] = [
+	'allowed_types'=>['xls','xlsx'],
+	'move_to' => ROOTPATH.'/var/tmp/',
+	'strip folder' => ROOTPATH,
 ];
