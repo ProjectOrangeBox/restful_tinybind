069e08be164676aec526e30403df196626972fb1
Don Myers <donmyers@icloud.com>
2019-01-23 14:52:29 -0500

    add new syntax

--- application/config/errors.php
+++ application/config/errors.php
@@ -4,7 +4,8 @@ $config['auto detect'] = true;
 
 $config['flashdata session variable'] = 'ci_errors';
 
-$config['html_prefix'] = '<p class="orange error">';
+$config['html_prefix'] = '<p class="{group class} orange-errors">';
 $config['html_suffix'] = '</p>';
+$config['html_group_class'] = '{group class}';
 
 $config['default error group'] = 'records';
