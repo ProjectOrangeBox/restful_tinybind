b74f079e6205df0fa0ad2ad80b9e9a3faed7846f
Don Myers <donmyers@quadratec.com>
2019-05-01 12:39:22 -0400

    add nav middleware on output

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,9 +1,12 @@
 <?php
-/* NOTE: this uses the FIRST match and stops */
+/**
+ * NOTE: this uses the FIRST match and stops
+ */
 
 $route = array(
 	'vendor_stock/(.*)' => function ($url, $router) {
-		$router->on_request('PublicMiddleware', 'GuiMiddleware', 'NavbarMiddleware');
+		$router->on_request('PublicMiddleware', 'GuiMiddleware');
+		$router->on_responds('NavbarMiddleware');
 
 		return 'vendor_stock/vendor/'.$url;
 	},
@@ -13,19 +16,22 @@ $route = array(
   },
 
   'admin/(.*)' => function ($url, $router) {
-	  $router->on_request('AdminMiddleware', 'PublicMiddleware', 'GuiMiddleware', 'NavbarMiddleware');
+	  $router->on_request('AdminMiddleware', 'PublicMiddleware', 'GuiMiddleware');
+		$router->on_responds('NavbarMiddleware');
 
 	  return 'admin/'.$url;
   },
 
   'login(.*)' => function ($url, $router) {
-	  $router->on_request('LoginMiddleware', 'PublicMiddleware', 'GuiMiddleware', 'NavbarMiddleware');
+	  $router->on_request('LoginMiddleware', 'PublicMiddleware', 'GuiMiddleware');
+		$router->on_responds('NavbarMiddleware');
 
 	  return 'login'.$url;
   },
 
   '(.*)' => function ($url, $router) {
-	  $router->on_request('PublicMiddleware', 'GuiMiddleware', 'NavbarMiddleware');
+	  $router->on_request('PublicMiddleware', 'GuiMiddleware');
+		$router->on_responds('NavbarMiddleware');
 
 	  return $url;
   },
