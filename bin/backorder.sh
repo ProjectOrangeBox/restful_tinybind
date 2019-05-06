196c43e3e31dc50d6f136d62fad4b1ae9ef3d962
Don Myers <donmyers@quadratec.com>
2019-05-06 12:10:09 -0400

    update to dynamically figure out root path

--- bin/backorder.sh
+++ bin/backorder.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-/usr/bin/flock -n /tmp/backorder_update_records.lockfile /usr/bin/php /var/www/skynet/app/public/index.php cli/backorder/records
-/usr/bin/flock -n /tmp/backorder_update_dates.lockfile /usr/bin/php /var/www/skynet/app/public/index.php cli/backorder/dates
-/usr/bin/flock -n /tmp/backorder_syncroles.lockfile /usr/bin/php /var/www/skynet/app/public/index.php cli/backorder/syncroles
+ROOT="`dirname $0`"
+ROOT="`dirname $ROOT`"
+
+echo $ROOT
+
+/usr/bin/flock -n /tmp/backorder_update_records.lockfile /usr/bin/php "$ROOT/public/index.php" cli/backorder/records
+/usr/bin/flock -n /tmp/backorder_update_dates.lockfile /usr/bin/php "$ROOT/public/index.php" cli/backorder/dates
+/usr/bin/flock -n /tmp/backorder_syncroles.lockfile /usr/bin/php "$ROOT/public/index.php" cli/backorder/syncroles
