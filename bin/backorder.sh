7faa7e9a381b53e19beb3ecdee1ef760663b53a7
Don Myers <donmyers@quadratec.com>
2019-05-06 12:37:14 -0400

    updates

--- bin/backorder.sh
+++ bin/backorder.sh
@@ -5,6 +5,11 @@ ROOT="`dirname $ROOT`"
 
 echo $ROOT
 
+echo "backorder/records"
 /usr/bin/flock -n /tmp/backorder_update_records.lockfile /usr/bin/php "$ROOT/public/index.php" cli/backorder/records
+
+echo "backorder/dates"
 /usr/bin/flock -n /tmp/backorder_update_dates.lockfile /usr/bin/php "$ROOT/public/index.php" cli/backorder/dates
+
+echo "backorder/syncroles"
 /usr/bin/flock -n /tmp/backorder_syncroles.lockfile /usr/bin/php "$ROOT/public/index.php" cli/backorder/syncroles
