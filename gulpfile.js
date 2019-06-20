65efd3b069e090511233a3dc7af2f6200859e86d
Don Myers <donmyers@mac.com>
2019-06-20 12:27:09 -0400

    update gulp with watchers and better config organizing

--- gulpfile.js
+++ gulpfile.js
@@ -16,116 +16,122 @@ sass.compiler = require('node-sass');
 const tempFolder = 'var/gulp';
 const distFolder = 'public/dist';
 
-let scss = [
-	'assets/css/application.scss'
+let pugViews = [
+	'application/views/**/*.pug',
 ];
 
-let css = [
-	/* vendor */
-	'node_modules/font-awesome/css/font-awesome.css',
-	'node_modules/bootstrap3/dist/css/bootstrap.css',
-	'node_modules/bootstrap3/dist/css/bootstrap-theme.css',
-	'node_modules/bootstrap-select/dist/css/bootstrap-select.css',
-	'node_modules/roboto-fontface/css/roboto/roboto-fontface.css',
+let css = {
+	'vendor': [
+		'node_modules/font-awesome/css/font-awesome.css',
+		'node_modules/bootstrap3/dist/css/bootstrap.css',
+		'node_modules/bootstrap3/dist/css/bootstrap-theme.css',
+		'node_modules/bootstrap-select/dist/css/bootstrap-select.css',
+		'node_modules/roboto-fontface/css/roboto/roboto-fontface.css',
+	],
+	'user': [
+		'assets/plugins/notify/notify.css',
+		'assets/plugins/table_sort/table_sort.css',
+	],
+	'scss': [
+		'assets/css/application.scss'
+	],
+};
 
-	/* user */
-	'assets/plugins/notify/notify.css',
-	'assets/plugins/table_sort/table_sort.css',
-];
+let js = {
+	'vendor': [
+		'node_modules/jquery/dist/jquery.min.js',
+		'node_modules/bootstrap3/dist/js/bootstrap.min.js',
+		'node_modules/bootstrap-select/dist/js/bootstrap-select.min.js',
+		'node_modules/tinysort/dist/tinysort.min.js',
+	],
+	'user': [
+		'assets/js/superStorage.js',
+		'assets/js/tinybind.js',
+		'assets/js/sprintf.js',
+		'assets/js/application.js',
+		'assets/js/config.js',
+		'assets/js/binders.js',
+		'assets/js/formatters.js',
+		'assets/js/orangeBind.js',
 
-let jsCombined = [
-	'node_modules/jquery/dist/jquery.min.js',
-	'node_modules/bootstrap3/dist/js/bootstrap.min.js',
-	'node_modules/bootstrap-select/dist/js/bootstrap-select.min.js',
-	'node_modules/tinysort/dist/tinysort.min.js',
-];
+		'assets/plugins/notify/notify.js',
+		'assets/plugins/bound-table-search/bound-table-search.js',
+		'assets/plugins/table_remember_position/table_remember_position.js',
+		'assets/plugins/table_sort/table_sort.js',
+		'assets/plugins/table_sticky_header/jquery.stickytableheaders.js',
 
-let jsUser = [
-	'assets/js/superStorage.js',
-	'assets/js/tinybind.js',
-	'assets/js/sprintf.js',
-	'assets/js/application.js',
-	'assets/js/config.js',
-	'assets/js/binders.js',
-	'assets/js/formatters.js',
-	'assets/js/orangeBind.js',
+		'assets/app/app.js',
 
-	'assets/plugins/notify/notify.js',
-	'assets/plugins/bound-table-search/bound-table-search.js',
-	'assets/plugins/table_remember_position/table_remember_position.js',
-	'assets/plugins/table_sort/table_sort.js',
-	'assets/plugins/table_sticky_header/jquery.stickytableheaders.js',
-
-	'assets/app/app.js',
-
-	'assets/js/onReady.js',
-];
+		'assets/js/onReady.js',
+	],
+};
 
 let copyDir = {
 	'node_modules/font-awesome/fonts': 'public/fonts',
 	'node_modules/roboto-fontface/fonts/roboto': 'public/fonts/roboto',
 };
 
-function compileJsVendor()
-{
-	return src(jsCombined)
-		.pipe(concat('vendor.js'))
+/* all config finished */
+
+/* auto build the watch arrays */
+let watchFiles = Array.prototype.concat(pugViews,css.user,css.vendor,css.scss,js.vendor,js.user);
+let watchFilesJs = Array.prototype.concat(js.user);
+let watchFilesCss = Array.prototype.concat(css.user,css.vendor,css.scss);
+let watchFilesPug = pugViews;
+
+function compilePug() {
+	return src(pugViews)
+		.pipe(pug({pretty:false}))
+		.pipe(dest('application/views'));
+}
+
+function compileJsVendor() {
+	return src(js.vendor)
+		.pipe(concat('1_vendor.js'))
 		.pipe(dest(tempFolder));
 }
 
-function compileJsUser()
-{
+function compileJsUser() {
 	/*
 	.pipe(sourcemaps.init())
 	.pipe(sourcemaps.write('.'))
 	*/
 
-	return src(jsUser)
+	return src(js.user)
 		.pipe(babel())
 		.pipe(uglify())
-		.pipe(concat('user.js'))
+		.pipe(concat('2_user.js'))
 		.pipe(dest(tempFolder));
 }
 
-function combinedJs()
-{
-	return src([tempFolder + '/vendor.js', tempFolder + '/user.js'])
+function combinedJs() {
+	return src(tempFolder + '/*.js')
 		.pipe(concat('bundle.js'))
 		.pipe(dest(distFolder));
 }
 
-function compileSass()
-{
-	return src(scss)
+function compileSass() {
+	return src(css.scss)
 		.pipe(sass())
-		.pipe(concat('sass.css'))
+		.pipe(concat('2_sass.css'))
 		.pipe(dest(tempFolder));
 }
 
-function compileCss()
-{
-	return src(css)
-		.pipe(concat('css.css'))
+function compileCss() {
+	return src(css.vendor)
+		.pipe(src(css.user))
+		.pipe(concat('1_css.css'))
 		.pipe(dest(tempFolder));
 }
 
-function combinedCss()
-{
+function combinedCss() {
 	return src(tempFolder + '/*.css')
  		.pipe(cleanCSS({compatibility: 'ie9'}))
 		.pipe(concat('bundle.css'))
 		.pipe(dest(distFolder));
 }
 
-function compilePug()
-{
-	return src('application/views/**/*.pug')
-		.pipe(pug({pretty:false}))
-		.pipe(dest('application/views'));
-}
-
-function copyDirectories(cb)
-{
+function copyDirectories(cb) {
 	/* as long as the others finish after this... */
 	for (var idx in copyDir) {
 		copy(__dirname + '/' + idx,__dirname + '/' + copyDir[idx],function(error, results) {});
@@ -134,9 +140,29 @@ function copyDirectories(cb)
 	return cb();
 }
 
+exports.watch = ()=>{
+	exports.default();
+	watch(watchFiles,parallel(series(compileJsUser,combinedJs),series(compileSass,compileCss,combinedCss),compilePug));
+}
+
+exports['watch.js'] = ()=>{
+	exports.default();
+	watch(watchFilesJs,series(compileJsUser,combinedJs));
+}
+
+exports['watch.css'] = ()=>{
+	exports.default();
+	watch(watchFilesCss,series(parallel(compileSass,compileCss),combinedCss));
+}
+
+exports['watch:pug'] = ()=>{
+	exports.default();
+	watch(watchFilesPug,parallel(compilePug));
+}
+
 exports.default = parallel(
 	copyDirectories,
-	series(compileJsVendor,compileJsUser,combinedJs),
-	series(compileSass,compileCss,combinedCss),
+	series(parallel(compileJsVendor,compileJsUser),combinedJs),
+	series(parallel(compileSass,compileCss),combinedCss),
 	compilePug,
 );
