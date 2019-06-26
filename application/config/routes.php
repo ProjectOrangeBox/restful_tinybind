d353d6bd4d2c09400d3ee9bb860672140773bbfc
Don Myers <donmyers@mac.com>
2019-06-26 09:05:12 -0400

    do all magic in controller remap

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,40 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 
-$route['default_controller'] = 'get';
-
-$isAjax = (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
-$isJson = (!empty($_SERVER['HTTP_ACCEPT']) && strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/json') !== false);
-
-if ($isAjax || $isJson) {
-	/* handle REST http methods */
-	$route['robot/create']['POST'] = 'robot/createPost';
-	$route['robot/edit/(:num)']['PATCH'] = 'robot/editPatch/$1';
-	$route['robot/delete/(:num)']['DELETE'] = 'robot/deleteDelete/$1';
-
-	$route['catalog/create']['POST'] = 'catalog/createPost';
-	$route['catalog/edit/(:num)']['PATCH'] = 'catalog/editPatch/$1';
-	$route['catalog/delete/(:num)']['DELETE'] = 'catalog/deleteDelete/$1';
-
-	$route['zipcodes/create']['POST'] = 'zipcodes/createPost';
-	$route['zipcodes/edit/(:num)']['PATCH'] = 'zipcodes/editPatch/$1';
-	$route['zipcodes/delete/(:num)']['DELETE'] = 'zipcodes/deleteDelete/$1';
-
-	$route['people/create']['POST'] = 'people/createPost';
-	$route['people/edit/(:num)']['PATCH'] = 'people/editPatch/$1';
-	$route['people/delete/(:num)']['DELETE'] = 'people/deleteDelete/$1';
-
-	$route['multi/create']['POST'] = 'multi/createPost';
-	$route['multi/edit/(:num)']['PATCH'] = 'multi/editPatch/$1';
-	$route['multi/delete/(:num)']['DELETE'] = 'multi/deleteDelete/$1';
-
-	$route['(:any)'] = 'robot';
-} else {
-	$route['test'] = 'test/index';
-
-	/* reguardless of the URL use the default */
-	$route['(.*)'] = $route['default_controller'];
-}
+$route['default_controller'] = 'main/index';
 
 $route['404_override'] = '';
 
